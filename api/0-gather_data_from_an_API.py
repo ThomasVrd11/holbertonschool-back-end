21f86059e760c3f9380600bb28d421bd6bae2796
ThomasVrd11 <Thomasviard@orange.fr>
2024-04-15 10:43:24 +0200

    Refactor script to gather data from an API in api/0-gather_data_from_an_API.py

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -10,32 +10,30 @@ if __name__ == "__main__":
         """ If the number of arguments is different from 2, print the error message"""
         print("Usage: {} <employee_id>".format(sys.argv[1]))
         sys.exit(1)
-        
+
     URL = "https://jsonplaceholder.typicode.com"
-    
-    
+
     EMPLOYEE_ID = sys.argv[1]
     """ Get the user info, the user name, and the number of tasks"""
-    
-    
+
     RESPONSE = requests.get(
         f"{URL}/users/{EMPLOYEE_ID}/todos",
         params={"_expand": "user"}
     )
     data = RESPONSE.json()
     """ It returns a list of dictionaries, where each dictionary represents a task"""
-    
+
     """ error handling, if the response is empty, print the error message and exit the program"""
     if not len(data):
         print("Resource not found")
         sys.exit(1)
-        
+
     """ This is the data that we need to extract from the response"""
     TASK_TITLE = [task["title"] for task in data if task["completed"]]
     TOTAL_NUMBER_OF_TASKS = len(data)
     NUMBER_OF_DONE_TASKS = len(TASK_TITLE)
     EMPLOYEE_NAME = data[0]["user"]["name"]
-    
+
     """ Print the data in the format specified in the task description
     The format is as follows:
     Employee EMPLOYEE_NAME is done with tasks(NUMBER_OF_DONE_TASKS/TOTAL_NUMBER_OF_TASKS):
@@ -43,7 +41,6 @@ if __name__ == "__main__":
         TASK_TITLE
         ..."""
     print(f"Employee {EMPLOYEE_NAME} is done with tasks"
-        f"({NUMBER_OF_DONE_TASKS}/{TOTAL_NUMBER_OF_TASKS}):")
+          f"({NUMBER_OF_DONE_TASKS}/{TOTAL_NUMBER_OF_TASKS}):")
     for i in TASK_TITLE:
         print("\t ", i)
-        
